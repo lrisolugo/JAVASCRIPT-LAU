b851cae0538b61f0edda0d15a4e13eeeb6c338f6
lrisolugo <146779067+lrisolugo@users.noreply.github.com>
2023-12-12 12:47:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+let PlatoPrincipal = 'Pizza Hawaiana';
+
+let PlatoSegudno = 'Pizza Margarita';
+
+const restaurant = 'Hard Rock';
+
+PlatoPrincipal = 'Pizza Calabreza'
+
+let PorcionesDePizza = 2;
+
+PorcionesDePizza = 3;
+
+
+let nombre = prompt("ingresa tu nombre:");
+let apellido = prompt("ingresa tu apellido:");
+let NombreCompleto = nombre + " " + apellido;
+
+console.log("bienvenid@", NombreCompleto);
+
+
+let PlatoElegido = prompt('¿que quieres comer hoy?');
+
+alert('hoy es un lindo dia para comer'+ PlatoElegido);
+
+let confirmar = confirm("¿Confirmas que aceptas los terminos y condiciones?");
+
+const pizza = 'pizza';
+const bananas = 'bananas';
+
+if (pizza !== bananas) {
+  console.log('¡La pizza no es igual a las bananas!');
+} else {
+  console.log('La pizza es mejor que las bananas.');
+}
+
+
+if (pizza == bananas) {
+  console.log('¡La pizza no es igual a las bananas!');
+} else {
+  console.log('La pizza es mejor que las bananas.');
+}
+
+
